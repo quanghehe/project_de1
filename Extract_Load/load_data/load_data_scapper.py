4e9287ec74a6ad5638431d30814258b0fa3197d8
quanghehe <havietquangss@gmail.com>
2025-07-07 09:14:30 +0700

    update file compose

--- Extract_Load/load_data/load_data_scapper.py
+++ Extract_Load/load_data/load_data_scapper.py
@@ -6,17 +6,27 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import logging
 
-DATA_JSON_ADOPT = "Extract_Load/data/adopt.json"
-DATA_JSON_HAHORD = "Extract_Load/data/Harrods.json"
-DATA_JSON_LIQUIES = "Extract_Load/data/liquides.json"
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_JSON_ADOPT = os.path.join(BASE_DIR, "data", "adopt.json")
+DATA_JSON_HAHROD = os.path.join(BASE_DIR, "data", "Harrods.json")
+DATA_JSON_LIQUIES = os.path.join(BASE_DIR, "data", "liquides.json")
 
 load_dotenv()
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-DB_URI = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+env = os.getenv("ENV", "local")
+
+if env == "docker":
+    host = os.getenv("DB_HOST_DOCKER")
+    port = os.getenv("DB_PORT_DOCKER")
+else:
+    host = os.getenv("DB_HOST")
+    port = os.getenv("DB_PORT")
+
+DB_URI = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{host}:{port}/{os.getenv('DB_NAME')}"
 engine = create_engine(DB_URI)
 
 def extract_number(size_str):
@@ -77,5 +87,5 @@ def load_json_to_db(filepath: str):
     logger.info("Loaded %s data into DB!", os.path.basename(filepath))
     
 load_json_to_db(DATA_JSON_ADOPT)
-load_json_to_db(DATA_JSON_HAHORD)
+load_json_to_db(DATA_JSON_HAHROD)
 load_json_to_db(DATA_JSON_LIQUIES)
